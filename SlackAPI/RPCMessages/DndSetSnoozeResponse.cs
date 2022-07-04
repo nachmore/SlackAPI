c45d471bc89de79fa0beb53f5bb756fd8e568dd4
Oren Nachman <code@nachmore.com>
2022-07-04 08:48:23 -0700

    Add support for dnd.setSnooze and users.profile.set

---
+++ SlackAPI/RPCMessages/DndSetSnoozeResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackAPI
+{
+  [RequestPath("dnd.setSnooze")]
+  public class DndSetSnoozeResponse : Response
+  {
+  }
+}
